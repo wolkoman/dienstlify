7d44b59f89aa611dc416633e05248d75ec05282f
Manuel Wolkowitsch <e1650767@student.tuwien.ac.at>
2021-04-25 09:25:19 +0200

    Update action

--- app.ts
+++ app.ts
@@ -1,17 +1,20 @@
 import {DOMParser} from 'https://deno.land/x/deno_dom/deno-dom-wasm.ts';
 import { config } from "https://deno.land/x/dotenv/mod.ts";
 
-const send = (text:string) => fetch(`https://api.telegram.org/bot${config().telegramApiToken}/sendMessage`, {
+console.log(`Starting up... ${config().TEST}`)
+
+const dienststellenIds = [9, 16,17, 18];
+const send = (text:string) => fetch(`https://api.telegram.org/bot${config().TELEGRAMAPITOKEN}/sendMessage`, {
   method: "POST",
   headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({'chat_id': config().chatId, text })
-}).then(x => x.text()).then(console.log);
+  body: JSON.stringify({'chat_id': config().CHATID, text })
+}).then(x => x.text())
 
 const toName = (value:string) => value === "Gesperrt!" || value === "R�cksprache mit Mannschaft" ||value === "" || value === " Eintragen" ? null : value;
 const fetchPage = ({dienststellenId, month, year}:{dienststellenId: number, month: number, year: number}) =>
   fetch(
     `https://intranet-md.n.roteskreuz.at/intern/?itemid=87&id=${dienststellenId}&month=${month}&year=${year}`,
-    {headers: {Cookie: `PHPSESSID=${config().sessionId}`}}
+    {headers: {Cookie: `PHPSESSID=${config().SESSIONID}`}}
     )
   .then(x => x.text())
   .then(x => new DOMParser().parseFromString(x, "text/html"))
@@ -22,8 +25,6 @@ const fetchPage = ({dienststellenId, month, year}:{dienststellenId: number, mont
       .map(x => ({day: x[0].textContent, startTime: x[2].textContent.split(" - ")[0], driver: toName(x[4].textContent), san1: toName(x[5].textContent), san2: toName(x[6].textContent) }))
   }))
 
-const dienststellenIds = [9, 16,17, 18];
-
 const dates = Array(3)
   .fill(0)
   .map((_, index) => new Date(new Date().setMonth(new Date().getMonth() + index)))
@@ -33,7 +34,7 @@ Promise.all(
   dates
     .flatMap(date => dienststellenIds.map(dienststellenId => ({...date, dienststellenId})))
     .map(obj => fetchPage(obj))
-).then(pages => pages
+).then(x => {console.log(x, config().SESSIONID);return x;}).then(pages => pages
   .flatMap(page => page.days.map(day => ({...day, title: page.title})))
   .filter(duty => duty.driver && (!duty.san1 || !duty.san2))
   .sort((a,b) => a.day.split("").reverse().join("").localeCompare(b.day.split("").reverse().join("")))
